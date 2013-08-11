e75119f2a4937a8c0cb1f390f234bf363c776e7b
Tony Schick <schick.anthony@gmail.com>
2013-08-11 18:03:28 -0500

    configured database for heroku

--- schick/settings.py
+++ schick/settings.py
@@ -5,6 +5,8 @@ import os
 gettext = lambda s: s
 PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
 
+
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -14,14 +16,12 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(PROJECT_PATH, 'schickdatabase.sqlite'), # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
     }
 }
 
