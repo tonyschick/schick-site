ea9b9fc94dc43a813356c3c30ca635557505e72d
Tony Schick <schick.anthony@gmail.com>
2013-08-11 20:42:32 -0500

    trying again to configure for heroku

--- schick/settings.py
+++ schick/settings.py
@@ -23,15 +23,6 @@ DATABASES = {
     }
 }
 
-'''
-for Heroku configuration
-'''
-
-#import dj_database_url
-#DATABASES['default'] =  dj_database_url.config()
-
-# Allow all host headers
-ALLOWED_HOSTS = ['*']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -218,3 +209,27 @@ LOGGING = {
         },
     }
 }
+
+
+
+### for Heroku configuration ###
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
